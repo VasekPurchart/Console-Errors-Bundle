95dc9039581ad0f7a084f88d0f247c71c643e041
Vasek Purchart <me@vasekpurchart.cz>
2015-06-07 13:46:15 +0200

    DI config in DependencyInjection

--- src/DependencyInjection/ConsoleErrorsExtension.php
+++ src/DependencyInjection/ConsoleErrorsExtension.php
@@ -15,7 +15,7 @@ class ConsoleErrorsExtension extends \Symfony\Component\HttpKernel\DependencyInj
 	 */
 	public function load(array $configs, ContainerBuilder $container)
 	{
-		$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+		$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
 		$loader->load('services.yml');
 	}
 
