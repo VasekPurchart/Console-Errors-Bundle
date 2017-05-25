bdb3092ffaefa7f0b4ee0bbf13a94b09ece0a438
Vasek Purchart <me@vasekpurchart.cz>
2017-05-25 20:15:40 +0200

    fix missing logger definition when only logging errors

--- src/DependencyInjection/ConsoleErrorsExtension.php
+++ src/DependencyInjection/ConsoleErrorsExtension.php
@@ -38,6 +38,7 @@ class ConsoleErrorsExtension extends \Symfony\Component\HttpKernel\DependencyInj
 		);
 
 		$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
+		$loader->load('services.yml');
 		if ($mergedConfig[Configuration::SECTION_EXCEPTIONS][Configuration::PARAMETER_EXCEPTION_ENABLED]) {
 			$loader->load('exception_listener.yml');
 		}
