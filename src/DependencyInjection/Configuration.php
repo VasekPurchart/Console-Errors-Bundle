d07128f6a33f3b841b827cd450cf083a1609e213
Petr Duda <petrduda@seznam.cz>
2024-11-24 13:01:28 +0100

    do not use fluent interface for Symfony configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -5,6 +5,8 @@ declare(strict_types = 1);
 namespace VasekPurchart\ConsoleErrorsBundle\DependencyInjection;
 
 use Psr\Log\LogLevel;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\IntegerNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
@@ -39,48 +41,61 @@ class Configuration implements \Symfony\Component\Config\Definition\Configuratio
 		$treeBuilder = new TreeBuilder();
 		$rootNode = $treeBuilder->root($this->rootNode);
 
-		$rootNode
-			->children()
-				->arrayNode(self::SECTION_EXCEPTIONS)
-					->addDefaultsIfNotSet()
-					->children()
-						->scalarNode(self::PARAMETER_EXCEPTION_ENABLED)
-							->info('Enable logging for exceptions.')
-							->defaultTrue()
-							->end()
-						->append($this->createLogLevelNode(
-							self::PARAMETER_EXCEPTION_LOG_LEVEL,
-							'Log level with which exceptions should be logged (accepts string or integer values).',
-							self::DEFAULT_EXCEPTION_LOG_LEVEL
-						))
-						->integerNode(self::PARAMETER_EXCEPTION_LISTENER_PRIORITY)
-							->info('Priority with which the listener will be registered.')
-							->defaultValue(self::DEFAULT_EXCEPTION_LISTENER_PRIORITY)
-							->end()
-						->end()
-					->end()
-				->arrayNode(self::SECTION_EXIT_CODE)
-					->addDefaultsIfNotSet()
-					->children()
-						->scalarNode(self::PARAMETER_EXIT_CODE_ENABLED)
-							->info('Enable logging for non-zero exit codes.')
-							->defaultTrue()
-							->end()
-						->append($this->createLogLevelNode(
-							self::PARAMETER_EXIT_CODE_LOG_LEVEL,
-							'Log level with which exit codes should be logged (accepts string or integer values).',
-							self::DEFAULT_EXIT_CODE_LOG_LEVEL
-						))
-						->integerNode(self::PARAMETER_EXIT_CODE_LISTENER_PRIORITY)
-							->info('Priority with which the listener will be registered.')
-							->defaultValue(self::DEFAULT_EXIT_CODE_LISTENER_PRIORITY)
-							->end()
-						->end()
-					->end()
-				->end()
-			->end();
+		$rootNode->children()->append($this->createExceptionsNode(self::SECTION_EXCEPTIONS));
+		$rootNode->children()->append($this->createExitCodeNode(self::SECTION_EXIT_CODE));
 
 		return $treeBuilder;
+	}
+
+	private function createExceptionsNode(string $nodeName): ArrayNodeDefinition
+	{
+		$node = new ArrayNodeDefinition($nodeName);
+		$node->addDefaultsIfNotSet();
+		$node->children()->append($this->createEnabledNode(
+			self::PARAMETER_EXCEPTION_ENABLED,
+			'Enable logging for exceptions.'
+		));
+		$node->children()->append($this->createLogLevelNode(
+			self::PARAMETER_EXCEPTION_LOG_LEVEL,
+			'Log level with which exceptions should be logged (accepts string or integer values).',
+			self::DEFAULT_EXCEPTION_LOG_LEVEL
+		));
+		$node->children()->append($this->createListenerPriorityNode(
+			self::PARAMETER_EXCEPTION_LISTENER_PRIORITY,
+			self::DEFAULT_EXCEPTION_LISTENER_PRIORITY
+		));
+
+		return $node;
+	}
+
+	private function createExitCodeNode(string $nodeName): ArrayNodeDefinition
+	{
+		$node = new ArrayNodeDefinition($nodeName);
+		$node->addDefaultsIfNotSet();
+		$node->children()->append($this->createEnabledNode(
+			self::PARAMETER_EXIT_CODE_ENABLED,
+			'Enable logging for non-zero exit codes.'
+		));
+		$node->children()->append($this->createLogLevelNode(
+			self::PARAMETER_EXIT_CODE_LOG_LEVEL,
+			'Log level with which exit codes should be logged (accepts string or integer values).',
+			self::DEFAULT_EXIT_CODE_LOG_LEVEL
+		));
+		$node->children()->append($this->createListenerPriorityNode(
+			self::PARAMETER_EXIT_CODE_LISTENER_PRIORITY,
+			self::DEFAULT_EXIT_CODE_LISTENER_PRIORITY
+		));
+
+		return $node;
+	}
+
+	private function createEnabledNode(string $nodeName, string $info): ScalarNodeDefinition
+	{
+		$node = new ScalarNodeDefinition($nodeName);
+		$node->info($info);
+		$node->defaultTrue();
+
+		return $node;
 	}
 
 	/**
@@ -92,32 +107,37 @@ class Configuration implements \Symfony\Component\Config\Definition\Configuratio
 	private function createLogLevelNode(string $nodeName, string $info, $defaultValue): ScalarNodeDefinition
 	{
 		$node = new ScalarNodeDefinition($nodeName);
-		$node
-			->info($info)
-			->defaultValue($defaultValue)
-			->beforeNormalization()
-				->ifString()
-				->then(function (string $value): string {
-					return strtolower($value);
-				})
-				->end()
-			->validate()
-				->ifTrue(function ($value): bool {
-					switch (true) {
-						case is_int($value):
-							return false;
-						case is_string($value) && defined(LogLevel::class . '::' . strtoupper($value)):
-							return false;
-						default:
-							return true;
-					}
-				})
-				->thenInvalid(sprintf(
-					'Invalid log level value "%%s". Must be either value from %s or an integer.',
-					LogLevel::class
-				))
-				->end()
-			->end();
+		$node->info($info);
+		$node->defaultValue($defaultValue);
+		$node->beforeNormalization()
+			->ifString()
+			->then(function (string $value): string {
+				return strtolower($value);
+			});
+		$node->validate()
+			->ifTrue(function ($value): bool {
+				switch (true) {
+					case is_int($value):
+						return false;
+					case is_string($value) && defined(LogLevel::class . '::' . strtoupper($value)):
+						return false;
+					default:
+						return true;
+				}
+			})
+			->thenInvalid(sprintf(
+				'Invalid log level value "%%s". Must be either value from %s or an integer.',
+				LogLevel::class
+			));
+
+		return $node;
+	}
+
+	private function createListenerPriorityNode(string $nodeName, int $defaultValue): IntegerNodeDefinition
+	{
+		$node = new IntegerNodeDefinition($nodeName);
+		$node->info('Priority with which the listener will be registered.');
+		$node->defaultValue($defaultValue);
 
 		return $node;
 	}
